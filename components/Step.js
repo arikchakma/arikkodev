07f8773c646fd76d6f9a73c0d80f16e6e3044e42
Arik Chakma <arikchangma@gmail.com>
2021-11-19 02:36:00 +0600

    🎉 Initialised

---
+++ components/Step.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Step({ number, title }) {
+  const numberReplace = Number(number) < 10 ? `0${number}` : number;
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <NumberWrapper>{numberReplace}</NumberWrapper>
+        <TextWrapper>
+          <Point>{title}</Point>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
+}
+
+// Main Wrapper
+const Wrapper = styled.div``;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+// Number Wrapper
+const NumberWrapper = styled.div`
+  width: 32px;
+  height: 32px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 1000px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: -0.03125em;
+`;
+
+// Text Wrapper
+const TextWrapper = styled.div`
+  margin-left: 0.75rem;
+`;
+
+const Point = styled.h3`
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: -0.03125em;
+`;
