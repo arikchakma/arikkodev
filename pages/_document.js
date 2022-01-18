1b56c2f19f89dc2b3f793c307fcfe5be28832a4b
Arik Chakma <arikchangma@gmail.com>
2022-01-19 00:30:36 +0600

    Revert "Fixed nothing💔"

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-<<<<<<< Updated upstream
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -88,64 +87,4 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
-=======
-export default function Document(props) {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/ibm-plex-sans-var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
-        <link href="/static/favicons/site.webmanifest" rel="manifest" />
-        {/* <link
-          rel="preconnect"
-          href="https://cdn.usefathom.com"
-          crossOrigin=""
-        /> */}
-        <link
-          href="/static/favicons/apple-touch-icon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
-        <link
-          href="/static/favicons/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          href="/static/favicons/favicon-16x16.png"
-          rel="icon"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          color="#4a9885"
-          href="/static/favicons/safari-pinned-tab.svg"
-          rel="mask-icon"
-        />
-        <meta content="#ffffff" name="theme-color" />
-        <meta content="#ffffff" name="msapplication-TileColor" />
-        <meta
-          content="/static/favicons/browserconfig.xml"
-          name="msapplication-config"
-        />
-        {/* <meta content="14d2e73487fa6c71" name="yandex-verification" /> */}
-        {/* <meta
-          content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw"
-          name="google-site-verification"
-        /> */}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
->>>>>>> Stashed changes
 }
