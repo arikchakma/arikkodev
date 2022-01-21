6f27055b192160d969cd15b207e07d8d702652aa
Arik Chakma <arikchangma@gmail.com>
2022-01-22 05:53:03 +0600

    Removed JSX🎉

--- components/Step.js
+++ components/Step.js
@@ -1,52 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+// import React from 'react';
 
-export default function Step({ number, title }) {
-  const numberReplace = Number(number) < 10 ? `0${number}` : number;
-  return (
-    <Wrapper>
-      <ContentWrapper>
-        <NumberWrapper>{numberReplace}</NumberWrapper>
-        <TextWrapper>
-          <Point>{title}</Point>
-        </TextWrapper>
-      </ContentWrapper>
-    </Wrapper>
-  );
-}
+// export default function Step({ number, title }) {
+//   const numberReplace = Number(number) < 10 ? `0${number}` : number;
+//   return (
+//     <Wrapper>
+//       <ContentWrapper>
+//         <NumberWrapper>{numberReplace}</NumberWrapper>
+//         <TextWrapper>
+//           <Point>{title}</Point>
+//         </TextWrapper>
+//       </ContentWrapper>
+//     </Wrapper>
+//   );
+// }
 
-// Main Wrapper
-const Wrapper = styled.div``;
+// // Main Wrapper
+// const Wrapper = styled.div``;
 
-const ContentWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
+// const ContentWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
 
-// Number Wrapper
-const NumberWrapper = styled.div`
-  width: 32px;
-  height: 32px;
+// // Number Wrapper
+// const NumberWrapper = styled.div`
+//   width: 32px;
+//   height: 32px;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
 
-  border-radius: 1000px;
-  border: 1px solid rgba(0, 0, 0, 0.2);
+//   border-radius: 1000px;
+//   border: 1px solid rgba(0, 0, 0, 0.2);
 
-  font-weight: 700;
-  font-size: 16px;
-  letter-spacing: -0.03125em;
-`;
+//   font-weight: 700;
+//   font-size: 16px;
+//   letter-spacing: -0.03125em;
+// `;
 
-// Text Wrapper
-const TextWrapper = styled.div`
-  margin-left: 0.75rem;
-`;
+// // Text Wrapper
+// const TextWrapper = styled.div`
+//   margin-left: 0.75rem;
+// `;
 
-const Point = styled.h3`
-  font-size: 20px;
-  font-weight: 600;
-  letter-spacing: -0.03125em;
-`;
+// const Point = styled.h3`
+//   font-size: 20px;
+//   font-weight: 600;
+//   letter-spacing: -0.03125em;
+// `;
