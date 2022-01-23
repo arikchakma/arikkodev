0407d2fe546b365b8ccf03486d9c2aaf698598ee
Arik Chakma <arikchangma@gmail.com>
2022-01-23 23:02:51 +0600

    Removed GTAG🎉

--- pages/_document.js
+++ pages/_document.js
@@ -51,22 +51,6 @@ export default function Document(props) {
           content="6GngfjvZoTOZDrw-UgX-gcL2NNsLhFOGPSGaqVyOBTo"
           name="google-site-verification"
         />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `
-          }}
-        />
       </Head>
       <body>
         <Main />
