9946c5079e263b78176d0512680115e7738b7168
Arik Chakma <arikchangma@gmail.com>
2022-02-01 11:03:01 +0600

    Wallet Testing🎉

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
 import '../styles/globals.css';
 
 import { ThemeProvider } from 'next-themes';
+import {
+  Provider,
+  defaultChains,
+  defaultL2Chains,
+  developmentChains
+} from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
+// API key for Ethereum node
+// const infuraId = process.env.INFURA_ID;
+
+// Set up connectors
+const connectors = () => {
+  return [
+    new InjectedConnector({
+      chains: [...defaultChains, ...defaultL2Chains, ...developmentChains]
+    })
+  ];
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <Provider
+        autoConnect
+        connectorStorageKey="mirror.wallet"
+        connectors={connectors}
+      >
+        <Component {...pageProps} />
+      </Provider>
     </ThemeProvider>
   );
 }
---
+++ pages/wallet.js
@@ -0,0 +1,36 @@
+import { useAccount, useConnect } from 'wagmi';
+
+export default function Wallet() {
+  const [{ data, error }, connect] = useConnect();
+  const [{ data: accountData }, disconnect] = useAccount({
+    fetchEns: true
+  });
+
+  if (accountData) {
+    return (
+      <div>
+        <img src={accountData.ens?.avatar} alt="ENS Avatar" />
+        <div>
+          {accountData.ens?.name
+            ? `${accountData.ens?.name} (${accountData.address})`
+            : accountData.address}
+        </div>
+        <div>Connected to {accountData.connector.name}</div>
+        <button onClick={disconnect}>Disconnect</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-5">
+      {data.connectors.map(x => (
+        <button key={x.id} onClick={() => connect(x)}>
+          {x.name}
+          {!x.ready && ' (unsupported)'}
+        </button>
+      ))}
+
+      {error && <div>{error?.message ?? 'Failed to connect'}</div>}
+    </div>
+  );
+}
