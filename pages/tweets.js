2d9698e3d7653a8cf895051885f8513966af3945
Arik Chakma <arikchangma@gmail.com>
2022-02-23 04:14:41 +0600

    Added Twitter page

---
+++ pages/tweets.js
@@ -0,0 +1,11 @@
+import Container from '@/components/Container';
+import Divider from '@/components/Divider';
+
+export default function Tweets() {
+  return (
+    <Container>
+      <div>Hello, Twitter Gang!</div>
+      <Divider />
+    </Container>
+  );
+}
