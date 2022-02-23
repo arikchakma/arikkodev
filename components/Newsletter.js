ed0146891f7e6d4cc7d385039522d9ef3ebe9d06
Arik Chakma <arikchangma@gmail.com>
2022-02-23 08:33:56 +0600

    Fixed Confetti bug

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -1,8 +1,17 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback } from 'react';
+import ReactCanvasConfetti from 'react-canvas-confetti';
 import cn from 'classnames';
 import SuccessMessage from './SuccessMessage';
 import ErrorMessage from './ErrorMessage';
-import Realistic from './RealisticFireWorks';
+
+const canvasStyles = {
+  position: 'fixed',
+  pointerEvents: 'none',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0
+};
 
 export default function Newsletter({ blog }) {
   const [formState, setFormState] = useState({
@@ -12,6 +21,50 @@ export default function Newsletter({ blog }) {
     state: false
   });
   const inputEl = useRef();
+
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback(instance => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55
+    });
+
+    makeShot(0.2, {
+      spread: 60
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45
+    });
+  }, [makeShot]);
 
   const subscribe = async e => {
     e.preventDefault();
@@ -47,6 +100,7 @@ export default function Newsletter({ blog }) {
       success: true,
       state: true
     });
+    fire();
   };
 
   return (
@@ -95,7 +149,7 @@ export default function Newsletter({ blog }) {
           ) : null}
         </div>
       </div>
-      <Realistic fireOn={formState?.success} />
+      <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
     </div>
   );
 }
