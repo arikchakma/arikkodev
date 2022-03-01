83bc4ddccbb9c3a3bfb56950a510e0663a794191
Arik Chakma <arikchangma@gmail.com>
2022-03-01 22:55:55 +0600

    Configured TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 
 import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
