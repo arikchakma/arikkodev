40620be1059557dbe1d32d51505f8b3ee7623109
Arik Chakma <arikchangma@gmail.com>
2022-06-01 05:28:04 +0600

    Init w/ lint and formatting.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello</main>
+    </div>
+  );
+};
+
+export default Home;
