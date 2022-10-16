343cadea5eddd2d5b535bde7e61115bd0737efd7
Arik Chakma <arikchangma@gmail.com>
2022-10-17 02:34:31 +0600

    fix: removed unnecessary stuffs

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import countries from './lib/countries.json';
 
 // run only on homepage
 export const config = {
@@ -10,19 +9,9 @@ export async function middleware(req: NextRequest) {
   const { nextUrl: url, geo } = req;
   const country = geo?.country || 'US';
   const city = geo?.city || 'San Francisco';
-  const region = geo?.region || 'CA';
-
-  const countryInfo = countries.find(x => x.cca2 === country);
-  const flag = countryInfo?.flag;
-
-  // const currencyCode = Object.keys(countryInfo.currencies)[0];
-  // const currency = countryInfo.currencies[currencyCode];
-  // const languages = Object.values(countryInfo.languages).join(', ');
 
   url.searchParams.set('country', country);
   url.searchParams.set('city', city);
-  url.searchParams.set('region', region);
-  url.searchParams.set('flag', flag as string);
 
   return NextResponse.rewrite(url);
 }
