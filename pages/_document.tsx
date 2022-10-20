06996ce651c6445779d2720ce45417b61aa41bae
Arik Chakma <arikchangma@gmail.com>
2022-10-21 02:36:39 +0600

    chore: added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,36 @@ export default function Document(props: DocumentProps) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/static/favicons/safari-pinned-tab.svg"
+          color="#fcfcfc"
+        />
+        <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#fcfcfc" />
+        <meta
+          name="msapplication-config"
+          content="/static/favicons/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className="px-5 font-sans text-[#313233] antialiased selection:bg-[#95a5ac40]">
         <Main />
