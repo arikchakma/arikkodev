27cd8927f711f30df79fe6f7e6eb46b7fd136fff
Arik Chakma <arikchangma@gmail.com>
2022-10-23 03:20:21 +0600

    fix: top blur gap

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -2,7 +2,7 @@ import Link from '@/components/Link';
 
 export default function Header() {
   return (
-    <header className="mt-16">
+    <header className="mt-14">
       <nav className="">
         <ul className="flex items-center gap-3">
           <li>
