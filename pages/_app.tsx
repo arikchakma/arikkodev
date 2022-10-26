f329fb87832c3348517ae89b8dc06d38a1791c5f
Arik Chakma <arikchangma@gmail.com>
2022-10-26 19:17:59 +0600

    feat: vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // Provide the client to your App
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      <Analytics />
+    </>
   );
 }
 
