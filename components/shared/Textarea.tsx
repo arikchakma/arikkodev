00dc1f13bde4a8973be6bed110260eea8205cfbc
Arik Chakma <arikchangma@gmail.com>
2022-10-31 01:49:29 +0600

    feat: feedback base

---
+++ components/shared/Textarea.tsx
@@ -0,0 +1,33 @@
+import { forwardRef, HTMLAttributes } from 'react';
+import { FieldErrors } from 'react-hook-form';
+import cn from 'clsx';
+
+interface TextareaProps extends HTMLAttributes<HTMLTextAreaElement> {
+  rows: number;
+  error: FieldErrors;
+}
+
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  function textarea({ rows, error, placeholder, className, ...props }, ref) {
+    return (
+      <>
+        <textarea
+          {...props}
+          ref={ref}
+          placeholder={placeholder}
+          rows={rows}
+          className={cn(
+            'block w-full rounded-md border-none bg-link-color/5 p-2 outline-none focus:ring-1 focus:ring-indigo-500',
+            error.message ? 'ring-1 ring-red-500' : '',
+            className
+          )}
+        />
+        {error.message ? (
+          <p className="mt-1 text-sm text-red-500">{`${error.message.message}`}</p>
+        ) : null}
+      </>
+    );
+  }
+);
+
+export default Textarea;
