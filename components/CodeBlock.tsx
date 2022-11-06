031c6d9901f85241da1e14c2b3fad9ff6c4eafa1
Arik Chakma <arikchangma@gmail.com>
2022-11-06 21:27:44 +0600

    fix: copy button  hover animation

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -24,19 +24,21 @@ export default function CodeBlock({
   }, []);
 
   return (
-    <pre {...props} className={cn('relative', className)}>
-      <code ref={codeRef}>{children}</code>
-      <button
-        aria-label="Copy code to clipboard"
-        onClick={() => {
-          copy(code);
-          setHasCopied(true);
-          setTimeout(() => setHasCopied(false), 2000);
-        }}
-        className="absolute top-2 right-2 inline-flex h-6 w-6 items-center justify-center rounded-md bg-gray-100 text-gray-300 ring-1  ring-gray-200 transition-all duration-200 hover:bg-gray-200 hover:text-gray-400 hover:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-      >
-        {hasCopied ? <CheckIcon /> : <ClipboardIcon className="" />}
-      </button>
-    </pre>
+    <div className="group">
+      <pre {...props} className={cn('relative', className)}>
+        <code ref={codeRef}>{children}</code>
+        <button
+          aria-label="Copy code to clipboard"
+          onClick={() => {
+            copy(code);
+            setHasCopied(true);
+            setTimeout(() => setHasCopied(false), 2000);
+          }}
+          className="absolute top-2 right-2 inline-flex h-6 w-6 items-center justify-center rounded-md bg-gray-100 text-gray-300 opacity-0 ring-1  ring-gray-200 transition-all duration-200 hover:bg-gray-200 hover:text-gray-400 hover:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-100 group-hover:opacity-100"
+        >
+          {hasCopied ? <CheckIcon /> : <ClipboardIcon className="" />}
+        </button>
+      </pre>
+    </div>
   );
 }
