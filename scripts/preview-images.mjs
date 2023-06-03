b57e7647d60ebca44f10c4bba59c9c97cfcc09e5
Arik Chakma <arikchangma@gmail.com>
2023-06-03 21:39:56 +0600

    chore: content management

--- scripts/preview-images.mjs
+++ scripts/preview-images.mjs
@@ -1,10 +1,13 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { chromium } from 'playwright-core';
 
 const allWritings = readFileSync(
   '.contentlayer/generated/Writing/_index.json',
   'utf8'
 );
+
+// Get all the file names from the public/previews folder
+const images = JSON.parse(readFileSync('public/previews/index.json', 'utf8'));
 
 const altLinks = [
   'https://arikko.dev',
@@ -14,14 +17,18 @@ const altLinks = [
   'https://preactjs.com/',
   'https://astro.build/',
   'https://github.com/kamranahmedse/developer-roadmap/blob/master/src/components/CommandMenu/CommandMenu.tsx',
+  'https://docs.astro.build/en/guides/imports/#astroglob',
+  'https://docs.astro.build/en/core-concepts/endpoints/#static-file-endpoints',
 ];
+const imagesList = [];
 
-function previewImages() {
-  JSON.parse(allWritings).map(writing => {
+async function previewImages() {
+  const browser = await chromium.launch();
+  await Promise.all(JSON.parse(allWritings).map(writing => {
     writing.externalLinks.map(async externalLink => {
       if (!externalLink) return;
-      let browser = await chromium.launch();
 
+      if (images.images.includes(externalLink.name)) return;
       let page = await browser.newPage();
       await page.setViewportSize({ width: 1440, height: 1080 });
       await page.emulateMedia({ colorScheme: 'dark' });
@@ -30,23 +37,37 @@ function previewImages() {
       await page.screenshot({
         path: `public/previews/${externalLink.name}.png`,
       });
-      await browser.close();
+      imagesList.push(externalLink.name);
+      await page.close();
     });
-  });
-  altLinks.map(async link => {
-    const name = link.replace(/\//g, '@').replace(/#/g, '@');
-    let browser = await chromium.launch();
+  }))
+  await Promise.all(
+    altLinks.map(async link => {
+      const name = link.replace(/\//g, '@').replace(/#/g, '@');
 
-    let page = await browser.newPage();
-    await page.setViewportSize({ width: 1440, height: 1080 });
-    await page.emulateMedia({ colorScheme: 'dark' });
-    await page.goto(link);
-    await page.waitForTimeout(1000);
-    await page.screenshot({
-      path: `public/previews/${name}.png`,
-    });
-    await browser.close();
-  });
+      if (images.images?.includes(name)) return;
+
+      let page = await browser.newPage();
+      await page.setViewportSize({ width: 1440, height: 1080 });
+      await page.emulateMedia({ colorScheme: 'dark' });
+      await page.goto(link);
+      await page.waitForTimeout(1000);
+      await page.screenshot({
+        path: `public/previews/${name}.png`,
+      });
+      await page.close();
+      imagesList.push(name);
+    })
+  );
+  await browser.close();
 }
 
-previewImages();
+previewImages().finally(() => {
+  console.log(imagesList)
+  writeFileSync(
+    'public/previews/index.json',
+    JSON.stringify({
+      images: [...images.images, ...imagesList],
+    })
+  );
+});
