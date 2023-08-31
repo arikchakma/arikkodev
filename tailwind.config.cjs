3f483f8ef6d9492dcc448ac627b1522210d22687
Arik Chakma <arikchangma@gmail.com>
2023-08-31 23:45:35 +0600

    wip: about page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#313233',
+        selection: '#95a5ac40',
+      },
       fontFamily: {
         sans: ['Inter', ...fontFamily.sans],
       },
