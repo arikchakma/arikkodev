6c8f2b5dc128d93fee3fb6e145b4d6bc0f19bef8
Arik Chakma <arikchangma@gmail.com>
2023-09-01 06:32:56 +0600

    wip: writing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,40 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
+import rehypeSlug from 'rehype-slug'; // Add id to headings
+import rehypeCodeTitles from 'rehype-code-titles'; // Code Title
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'; // Add links to headings
+import rehypePrism from 'rehype-prism-plus'; // Syntax highlighting
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  markdown: {
+    syntaxHighlight: 'prism',
+    gfm: true,
+    rehypePlugins: [
+      rehypeCodeTitles,
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'wrap',
+          properties: {
+            className: ['anchor'],
+          },
+        },
+      ],
+      [
+        rehypePrism,
+        {
+          ignoreMissing: true,
+          showLineNumbers: true,
+          transformInlineCode: true,
+        },
+      ],
+    ],
+  },
+  integrations: [tailwind(), react(), mdx({})],
   site: 'https://arikko.dev',
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,4 @@
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import { fontFamily, spacing } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,6 +10,7 @@ module.exports = {
         selection: '#95a5ac40',
         background: '#fcfcfc',
         divider: '#5d676a',
+        'link-hover': 'rgb(93 103 106 / 0.7)',
       },
       fontFamily: {
         sans: ['Inter', ...fontFamily.sans],
@@ -27,7 +28,46 @@ module.exports = {
           },
         },
       },
+
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: '#313233',
+            a: {
+              color: theme('colors.divider'),
+              'text-decoration-color': 'rgb(93 103 106 / 0.6)',
+              ' text-underline-offset': '2px',
+              '&:hover': {
+                opacity: 0.7,
+              },
+              code: { color: theme('colors.divider') },
+            },
+            'h2,h3,h4': {
+              'font-weight': '700',
+              'margin-top': '2em',
+              'margin-bottom': '1em',
+              'line-height': '1.3333',
+              'letter-spacing': '-.02em',
+              'font-variation-settings': "'wght' 700",
+              'scroll-margin-top': spacing[32],
+            },
+            li: {
+              'margin-top': '0.5em',
+              'margin-bottom': '0.5em',
+            },
+            thead: {
+              borderBottomColor: theme('colors.gray.200'),
+            },
+            code: { color: theme('colors.pink.500') },
+            'blockquote p:first-of-type::before': false,
+            'blockquote p:last-of-type::after': false,
+            strong: {
+              'font-variation-settings': "'wght' 600",
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
