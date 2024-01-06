3d986861df98969ecbd258e84c7a12654a40b623
Arik Chakma <arikchangma@gmail.com>
2024-01-06 18:51:59 +0600

    feat: add project Sankhya

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly SANKHYA_ANALYTICS_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
