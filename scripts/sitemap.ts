15b2d143dc9df7117c6d0b345318cb91fe62890f
Arik Chakma <arikchangma@gmail.com>
2026-04-06 21:55:31 +0600

    Building on Cloudflare article + SEO improvements + package upgrades (#28)
    * feat: add building on cloudflare article
    
    * feat: add json-ld schema and improve sitemap
    
    * chore: upgrade packages and fix vercel adapter import
    
    * feat: improve article description

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -1,18 +1,31 @@
 import path from 'path';
 import fs from 'fs';
+import matter from 'gray-matter';
 import type { SitemapItem } from '@astrojs/sitemap';
+import { EnumChangefreq } from 'sitemap';
 
-export async function getWritingIds() {
-  const files = fs.readdirSync(
-    path.join(process.cwd(), 'src/content/writings'),
-  );
+type Writing = {
+  id: string;
+  publishedAt: Date | undefined;
+};
 
-  // Only include mdx files
-  const writingIds = files
+export async function getPublishedWritings(): Promise<Writing[]> {
+  const dir = path.join(process.cwd(), 'src/content/writings');
+  const files = fs.readdirSync(dir);
+
+  return files
     .filter((file) => path.extname(file) === '.mdx')
-    // Remove the .mdx extension to get the id of the writing
-    .map((file) => file.replace(/\.mdx$/, ''));
-  return writingIds;
+    .flatMap((file) => {
+      const { data } = matter.read(path.join(dir, file));
+
+      if (data.status === 'draft') {
+        return [];
+      }
+
+      return [
+        { id: file.replace(/\.mdx$/, ''), publishedAt: new Date(data.publishedAt) },
+      ];
+    });
 }
 
 export function shouldIndexPage(pageUrl: string) {
@@ -22,25 +35,34 @@ export function shouldIndexPage(pageUrl: string) {
 export async function serializeSitemap(
   item: SitemapItem,
 ): Promise<SitemapItem | undefined> {
-  const highPriorityPages = [
+  const writings = await getPublishedWritings();
+
+  const staticHighPriorityPages = [
     'https://arikko.dev',
     'https://arikko.dev/projects',
     'https://arikko.dev/writings',
-    'https://arikko.dev/notes',
-    ...(await getWritingIds()).flatMap((id) => [
-      `https://arikko.dev/writings/${id}`,
-    ]),
   ];
 
-  for (let pageUrl of highPriorityPages) {
-    if (item.url === pageUrl) {
-      return {
-        ...item,
-        // @ts-ignore
-        changefreq: 'monthly',
-        priority: 1,
-      };
-    }
+  if (staticHighPriorityPages.includes(item.url)) {
+    return {
+      ...item,
+      changefreq: EnumChangefreq.MONTHLY,
+      priority: 1,
+    };
   }
+
+  const writing = writings.find(
+    (w) => item.url === `https://arikko.dev/writings/${w.id}`,
+  );
+
+  if (writing) {
+    return {
+      ...item,
+      changefreq: EnumChangefreq.MONTHLY,
+      priority: 1,
+      lastmod: writing.publishedAt?.toISOString() || undefined,
+    };
+  }
+
   return undefined;
 }
